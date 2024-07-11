842e5d0995267d25251b2e9434acc7f54da0dc90
DESKTOP-NNN595M\pc <phanhuutin3052004@gmail.com>
2024-07-11 19:19:45 +0700

    done

--- site/view/contact/form.php
+++ site/view/contact/form.php
@@ -13,7 +13,7 @@
         <div class="row contact">
             <div class="col-md-6">
                 <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.4981304733305!2d106.62440891371494!3d10.773108292323755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752dcdd60efd3f%3A0x8e16362a8a43158e!2zVGjhuqd5IEzhu5ljIGThuqF5IGzhuq1wIHRyw6xuaCB3ZWI!5e0!3m2!1svi!2s!4v1570486606187!5m2!1svi!2s"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.415732808003!2d106.57970327480609!3d10.8559507892977!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752a5f56a41d83%3A0x3f586e6a364dfc1f!2zMi8xQSDhuqVwIDIsIFh1w6JuIFRo4bubaSBUaMaw4bujbmcsIEjDs2MgTcO0biwgSOG7kyBDaMOtIE1pbmgsIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1720700175470!5m2!1svi!2s"
                     width="100%" height="400px" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
             </div>
             <div class="col-md-6">
